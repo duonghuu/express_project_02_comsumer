d96973252901b62a64f74179ad759621dc15b9fd
duonghuu <huuduong9@gmail.com>
2025-10-31 18:05:34 +0700

    feat: c

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,63 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 import { Worker } from "bullmq";
+import { ResponsysService } from "@services/responsysService";
 const worker = new Worker(
   "resapi_queue",
   async (job) => {
-
-    const { type, data } = job.data;
-
+    const { data } = job.data;
     try {
+      let activityData = {
+        activity: data.activity ? data.activity : data.event_name,
+        customer_id: data.customer_id,
+        email_address: data.email_address,
+        mobile_number: data.mobile_number,
+        user_id: data.user_id,
+        event_name: data.event_name,
+        app_source: data.event_source,
+        timestamp: data.timestamp,
+        params: data.params,
+      };
+      ///
+      await ResponsysService.callActivityAPI(true, activityData.activity, activityData);
 
-      switch (type) {
-        case "ADD_ACTIVITY":
-          const config = {
-            method: 'post',
-            url: `${process.env.RESPONSYS_ENDPOINT}/rest/api/v1.3/folders/Banking/suppData/Activity_${data.activity}/members`,
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": null
-            },
-            data
-          };
-          let res = await axios(config);
-          const result = res.data;
-          console.log(result)
-          const insertResult = await CallResponsysService.create(result);
-
-          return result;
-
-        default:
-          throw new Error(`Không hỗ trợ job type: ${type}`);
+      let triggerData = {
+        //type: 'TRIGGER_EVENT',
+        event: data.event_name,
+        contact: {
+          customer_id: data.customer_id,
+          email_address: data.email_address,
+          mobile_number: data.mobile_number,
+        },
+        data: {
+          event_source: data.event_source,
+          ...data.params
+        }
       }
+      ///
+      await ResponsysService.callTriggerEventAPI(triggerData.event, triggerData, null)
+
+      // switch (type) {
+      //   case "ADD_ACTIVITY":
+      //     const config = {
+      //       method: 'post',
+      //       url: `${process.env.RESPONSYS_ENDPOINT}/rest/api/v1.3/folders/Banking/suppData/Activity_${data.activity}/members`,
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //         "Authorization": null
+      //       },
+      //       data
+      //     };
+      //     let res = await axios(config);
+      //     const result = res.data;
+      //     console.log(result)
+      // const insertResult = await CallResponsysService.create(result);
+
+      //     return result;
+
+      //   default:
+      //     throw new Error(`Không hỗ trợ job type: ${type}`);
+      // }
     } catch (err) {
       // Trả lỗi để BullMQ biết job này fail và retry
       throw err;
--- src/services/responsysService.ts
+++ src/services/responsysService.ts
@@ -2,12 +2,49 @@ import axios from "axios";
 import { ResponsysRepository } from "repositories/responsysRepository";
 import { CallResponsysService } from "./callResponsysService";
 import { v4 as uuidv4 } from 'uuid';
+// import * as CryptoJS from 'crypto-js';
 
+//         let hash = CryptoJS.SHA256(randomNID);
+//         const CUSTOMER_ID_ = hash.toString(CryptoJS.enc.Base64);
+// var decrypted = CryptoJS.AES.decrypt(process.env.RESPONSYS_PASS, process.env.RESPONSYS_KEY);
+// var password = decrypted.toString(CryptoJS.enc.Utf8);
 export const ResponsysService = {
     authToken: null,
     endPoint: process.env.RESPONSYS_ENDPOINT, // Replace with actual endpoint
     API_PROFILE_LIST: 'Resp_Banking_Customers',
-
+    API_FOLDER: 'Banking',
+    /*
+        async authenticate() {
+    
+            let base_url = ""
+            let user_name = process.env.RESPONSYS_USERNAME;
+    
+            //call responsys api
+            try {
+                const config = {
+                    method: 'post',
+                    url: `${base_url}/rest/api/v1.3/auth/token`,
+                    headers: { "Content-Type": "application/x-www-form-urlencoded;charset=utf-8" },
+                    data: `user_name=${user_name}&password=${password}&auth_type=password`
+                };
+    
+                let res = await axios(config);
+                if (res.data && res.data.authToken) {
+                    authToken = res.data.authToken;
+                    endPoint = res.data.endPoint;
+                } else {
+                    console.log(res.data);
+                }
+    
+                return true;
+    
+            } catch (err) {
+                console.log('authenticate', err);
+            }
+    
+            return false;
+        }
+    */
     getDate(dateStr: string) {
         let timestamp = new Date(dateStr);
         return timestamp.getFullYear() + '/' + (timestamp.getMonth() + 1) + "/" + timestamp.getDate() + " " + timestamp.getHours() + ":" + timestamp.getMinutes() + ":" + timestamp.getSeconds();
@@ -26,7 +63,7 @@ export const ResponsysService = {
         return result;
     },
     async callActivityAPI(isCustomer: boolean, activityName: any, data: any) {
-        console.log('callActivityAPI', process.env)
+        // console.log('callActivityAPI', process.env)
         //call responsys api
         let result = {};
 
@@ -87,7 +124,7 @@ export const ResponsysService = {
         try {
             const config = {
                 method: 'post',
-                url: `localhost:6000/rest/api/v1.3/folders/Banking/suppData/Activity_${data.activity}/members`,
+                url: `${process.env.RESPONSYS_ENDPOINT}/rest/api/v1.3/folders/Banking/suppData/Activity_${activityName}/members`,
                 headers: {
                     "Content-Type": "application/json",
                     "Authorization": null
@@ -95,9 +132,9 @@ export const ResponsysService = {
                 data: postData
             };
 
-            // let res = await axios(config);
-            // result = res.data;
-            result = await CallResponsysService.create(config);
+            let res = await axios(config);
+            result = res.data;
+            await CallResponsysService.create(result);
 
             console.log('callActivityAPI :: success', activityName);
 
@@ -112,6 +149,97 @@ export const ResponsysService = {
                 result = await this.processActivity(isCustomer, activityName, data);
             }
         }
+    },
+    async processEvent(event: any, contact: any, data: any) {
+        // if (!authToken) {
+        //     await authenticate();
+        // }
+
+        let result = await this.callTriggerEventAPI(event, contact, data);
+        return result;
+    },
+    async callTriggerEventAPI(eventName: any, contact: any, data: any) {
+
+        //console.log('callTriggerEventAPI', eventName, contact, data);
+
+        //call responsys api
+        let result = {};
+
+        let optionalData = [];
+        for (let key in data) {
+            optionalData.push({
+                "name": key.toUpperCase(),
+                "value": data[key]
+            });
+        }
+
+        let recordData = {
+            "customEvent": {
+                "eventNumberDataMapping": null,
+                "eventDateDataMapping": null,
+                "eventStringDataMapping": null
+            },
+            "recipientData": [
+                {
+                    "recipient": {
+                        "customerId": contact.customer_id ? contact.customer_id : null,
+                        "emailAddress": contact.email_address ? contact.email_address : null,
+                        "listName": {
+                            "folderName": `${this.API_FOLDER}`,
+                            "objectName": `${this.API_PROFILE_LIST}`
+                        },
+                        "recipientId": null,
+                        "mobileNumber": contact.mobile_number ? contact.mobile_number : null,
+                        "emailFormat": "HTML_FORMAT"
+                    },
+                    "optionalData": optionalData
+                }
+            ]
+        };
+
+        try {
+            const config = {
+                method: 'post',
+                url: `${process.env.RESPONSYS_ENDPOINT}/rest/api/v1.3/events/${eventName}`,
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": null
+                },
+                data: recordData //JSON.stringify(data)
+            };
+
+            let res = await axios(config);
+            result = res.data;
+            console.log('callTriggerEventAPI :: success', res);
+            await CallResponsysService.create(result)
+
+            /*
+            if (result.success === true) {
+                //console.log('callTriggerEventAPI :: success', eventName);
+            } else {
+                //console.log('callTriggerEventAPI :: fail', eventName);
+    
+                if (result.success === false && result.recipientId === -1 && result.errorMessage === 'NO_RECIPIENT_FOUND') {
+    
+                    //console.log('call processContactNotFound');
+    
+                    return await processContactNotFound('event', eventName, contact, data);
+                }
+            }
+            */
+
+        } catch (err: any) {
+            console.log('callTriggerEventAPI :: error', err);
+
+            let errorData = err.response ? err.response.data : { message: "NO_RESPONSE_RETURN" };
+
+            //try recall if token expire
+            if (errorData.errorCode && errorData.errorCode === 'TOKEN_EXPIRED') {
+                this.authToken = null;
+                result = await this.processEvent(eventName, contact, data);
+            }
+        }
+        return result
     },
     async callContactAPI(pk1: string, pk2: string, data: any): Promise<any> {
         //call responsys api
